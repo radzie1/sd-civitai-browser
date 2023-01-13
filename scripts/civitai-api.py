e418ca6a8030f3257625cd9e5d0efc4935e89554
radzie1 <122233203+radzie1@users.noreply.github.com>
2023-01-14 06:45:19 +0700

    Update civitai-api.py

--- scripts/civitai-api.py
+++ scripts/civitai-api.py
@@ -11,10 +11,7 @@ from tqdm import tqdm
 import re
 from requests.exceptions import ConnectionError
 
-!apt-get install aria2 -y
-# !pip install aria2p
-!aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=8081 --disable-ipv6
-from aria2p import API
+
 def download_file(url, file_name):
     # Maximum number of retries
     max_retries = 5
@@ -23,90 +20,68 @@ def download_file(url, file_name):
     retry_delay = 10
 
     while True:
-        try:
-            aria2 = API(
-                host="http://localhost", port=8081
-            )
-            gid = aria2.add_uris([url], options={"out": file_name})
-            aria2.wait_for_download(gid)
-            print(f"{file_name} successfully downloaded.")
+        # Check if the file has already been partially downloaded
+        if os.path.exists(file_name):
+            # Get the size of the downloaded file
+            downloaded_size = os.path.getsize(file_name)
+
+            # Set the range of the request to start from the current size of the downloaded file
+            headers = {"Range": f"bytes={downloaded_size}-"}
+        else:
+            downloaded_size = 0
+            headers = {}
+
+        # Split filename from included path
+        tokens = re.split(re.escape('\\'), file_name)
+        file_name_display = tokens[-1]
+
+        # Initialize the progress bar
+        progress = tqdm(total=1000000000, unit="B", unit_scale=True, desc=f"Downloading {file_name_display}", initial=downloaded_size, leave=False)
+
+        # Open a local file to save the download
+        with open(file_name, "ab") as f:
+            while True:
+                try:
+                    # Send a GET request to the URL and save the response to the local file
+                    response = requests.get(url, headers=headers, stream=True)
+
+                    # Get the total size of the file
+                    total_size = int(response.headers.get("Content-Length", 0))
+
+                    # Update the total size of the progress bar if the `Content-Length` header is present
+                    if total_size == 0:
+                        total_size = downloaded_size
+                    progress.total = total_size 
+
+                    # Write the response to the local file and update the progress bar
+                    for chunk in response.iter_content(chunk_size=1024):
+                        if chunk:  # filter out keep-alive new chunks
+                            f.write(chunk)
+                            progress.update(len(chunk))
+
+                    downloaded_size = os.path.getsize(file_name)
+                    # Break out of the loop if the download is successful
+                    break
+                except ConnectionError as e:
+                    # Decrement the number of retries
+                    max_retries -= 1
+
+                    # If there are no more retries, raise the exception
+                    if max_retries == 0:
+                        raise e
+
+                    # Wait for the specified delay before retrying
+                    time.sleep(retry_delay)
+
+        # Close the progress bar
+        progress.close()
+        downloaded_size = os.path.getsize(file_name)
+        # Check if the download was successful
+        if downloaded_size >= total_size:
+            print(f"{file_name_display} successfully downloaded.")
             break
-        except:
-            max_retries -= 1
-            if max_retries == 0:
-                raise Exception("Error: File download failed after multiple retries.")
-            time.sleep(retry_delay)
-
-# def download_file(url, file_name):
-#     # Maximum number of retries
-#     max_retries = 5
-
-#     # Delay between retries (in seconds)
-#     retry_delay = 10
-
-#     while True:
-#         # Check if the file has already been partially downloaded
-#         if os.path.exists(file_name):
-#             # Get the size of the downloaded file
-#             downloaded_size = os.path.getsize(file_name)
-
-#             # Set the range of the request to start from the current size of the downloaded file
-#             headers = {"Range": f"bytes={downloaded_size}-"}
-#         else:
-#             downloaded_size = 0
-#             headers = {}
-
-#         # Split filename from included path
-#         tokens = re.split(re.escape('\\'), file_name)
-#         file_name_display = tokens[-1]
-
-#         # Initialize the progress bar
-#         progress = tqdm(total=1000000000, unit="B", unit_scale=True, desc=f"Downloading {file_name_display}", initial=downloaded_size, leave=False)
-
-#         # Open a local file to save the download
-#         with open(file_name, "ab") as f:
-#             while True:
-#                 try:
-#                     # Send a GET request to the URL and save the response to the local file
-#                     response = requests.get(url, headers=headers, stream=True)
-
-#                     # Get the total size of the file
-#                     total_size = int(response.headers.get("Content-Length", 0))
-
-#                     # Update the total size of the progress bar if the `Content-Length` header is present
-#                     if total_size == 0:
-#                         total_size = downloaded_size
-#                     progress.total = total_size 
-
-#                     # Write the response to the local file and update the progress bar
-#                     for chunk in response.iter_content(chunk_size=1024):
-#                         if chunk:  # filter out keep-alive new chunks
-#                             f.write(chunk)
-#                             progress.update(len(chunk))
-
-#                     downloaded_size = os.path.getsize(file_name)
-#                     # Break out of the loop if the download is successful
-#                     break
-#                 except ConnectionError as e:
-#                     # Decrement the number of retries
-#                     max_retries -= 1
-
-#                     # If there are no more retries, raise the exception
-#                     if max_retries == 0:
-#                         raise e
-
-#                     # Wait for the specified delay before retrying
-#                     time.sleep(retry_delay)
-
-#         # Close the progress bar
-#         progress.close()
-#         downloaded_size = os.path.getsize(file_name)
-#         # Check if the download was successful
-#         if downloaded_size >= total_size:
-#             print(f"{file_name_display} successfully downloaded.")
-#             break
-#         else:
-#             print(f"Error: File download failed. Retrying... {file_name_display}")
+        else:
+            print(f"Error: File download failed. Retrying... {file_name_display}")
 
 #def download_file(url, file_name):
 #    # Download the file and save it to a local file
